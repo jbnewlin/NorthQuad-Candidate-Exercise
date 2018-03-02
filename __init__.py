923693f31c4381ab2414e275321ebfc43869aeaa
jbnewlin <jnewlin@purdue.edu>
2018-03-02 11:18:50 -0500

    Didn't commit the flask code

--- __init__.py
+++ __init__.py
@@ -20,5 +20,40 @@ def register():
     print password
     return json.dumps("Registered")
 
+@app.route("/login", methods=['POST'])
+def login():
+    post = request.get_json()
+    username = post.get('username')
+    password = post.get('password')
+
+    print username
+    print password
+    return json.dumps("Logged in")
+
+@app.route("/posts", methods=['GET'])
+def getPosts():
+    posts = [{
+        "id": 1,
+        "game": "Skyrim",
+        "username": "jnewlin",
+        "rating": 5,
+        "review": "It was great"
+    },
+    {
+        "id": 2,
+        "game": "Minecraft",
+        "username": "jnewlin",
+        "rating": 3,
+        "review": "It was okay"
+    },
+    {
+        "id": 3,
+        "game": "Cube World",
+        "username": "jnewlin",
+        "rating": 1,
+        "review": "It was bad"
+    }]
+    return posts
+
 if __name__ == "__main__":
     app.run()
